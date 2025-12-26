92f8a592081eb8c5c1957e0b50e340cc44d04de0
PHC <firetruck6421@gmail.com>
2025-12-26 21:57:11 +0800

    modified config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 // Automatically detect if running on localhost or production
 const API_BASE_URL = (window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost")
     ? "http://127.0.0.1:8000"
-    : "https://stock-market-dashboard-qieq.onrender.com"; // Render Backend URL 
+    : "https://stock-market-dashboard-q1eq.onrender.com"; // Render Backend URL 
